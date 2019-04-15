942a592cb613d6dd704e751ce995e5e5bfc44313
Your Name <arai>
2019-04-15 09:06:55 +0000

    all created controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,13 @@
+class ContactsController < ApplicationController
+  def top
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,25 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def status
+  end
+
+  def update
+  end
+end
