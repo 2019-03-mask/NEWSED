64619798203e1a3178e472d8bba0b3bc9417e696
rina0731 <rina.a0731@gmail.com>
2019-04-16 04:16:12 +0000

    association fix2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-　has_many :contacts
+  has_many :contacts
   has_many :favorites, dependent: :destroy
   has_many :purchase_histories
   has_many :reviews, dependent: :destroy
