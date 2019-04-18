254109a1997ea274408fbf3f1a1e6101e366a32a
rina0731 <rina.a0731@gmail.com>
2019-04-18 10:45:15 +0000

    all routing fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to users_top_path
+    redirect_to top_path
   end
 
 
