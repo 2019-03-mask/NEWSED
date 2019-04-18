a3fdbbe81ac97f2659acc9b61a518f293914e0a8
kimitashoichi <kimi.syou.koma@gmail.com>
2019-04-18 12:42:17 +0000

    create view item_index,item_used_index,item_show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.where("item_states = '新品'")
   end
 
   def index_used
+    @items = Item.where("item_states = '中古'")
   end
 
   def show
+    @item = Item.find(params[:id])
+    @songs = Song.where("disc_id = '#{@item.id}'")
   end
 
   def new
@@ -22,8 +26,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
+    item = Item.new
+    item.save(item_params)
     redirect_to items_path
   end
 
@@ -41,6 +45,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_type, :item_name, :price, :item_image, :stock, discs_attributes: [:id, :disc_name, :_destroy, songs_attributes: [:id, :song_title, :_destroy]])
+    params.require(:item).permit(:artist_id, :lable_id, :genre_id, :item_states, :item_type, :item_name, :price, :item_image, :stock, discs_attributes: [:id, :disc_name, :item_id,:_destroy, songs_attributes: [:id, :song_title, :disc_id, :_destroy]])
   end
 end
